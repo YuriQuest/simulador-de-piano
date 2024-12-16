546c5a129e000fa1ff1e3bb77a86c3e8fd09ee9c
YuriQuest <162909475+YuriQuest@users.noreply.github.com>
2024-12-16 17:30:31 -0300

    🚚 Chore, ⬆️ Update: arquivos movidos, rodando audio

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,14 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let audio = new Audio("./src/sounds/a.wav"); // por padrão, audio src e o "a"
+
+const playAudio =(key) => {
+    audio.src = `./src/sounds/${key}.wav` // rodando audio do src atravez da tecla precionada
+    audio.play(); // rodando audio
+};
+
 pianoKeys.forEach(key => {
-    
-})
+    // chamada da função playAudio passando o valor de data-key como argumento
+    key.addEventListener("click", () => playAudio(key.dataset.key));
+});
+
